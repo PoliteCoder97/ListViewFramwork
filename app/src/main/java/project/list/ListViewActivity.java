4a76a8cbef265768fa0fbe38d7d841fbb192336e
PoliteCoder97 <politecoder97@gmail.com>
2019-05-28 09:01:57 +0430

    Initial commit

---
+++ app/src/main/java/project/list/ListViewActivity.java
@@ -0,0 +1,18 @@
+package project.list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.politecoder97.listviewframwork.R;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.public_list);
+
+        
+
+    }
+}
